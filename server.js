a7c484211d4bad2dce3d6b8510ac21b3ed7a5321
Brad McKinney <brad.mckin@gmail.com>
2019-01-29 15:28:24 -1000

    adds analytic tracker middleware that logs all requests made and when

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const exphbs = require('express-handlebars');
 const methodOverride = require('method-override');
@@ -17,6 +18,18 @@ app.set('views', __dirname + '/views/templates');
 app.set('view engine', '.hbs');
 app.use(methodOverride('_method'));
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use((req, res, next) => {
+  let now = new Date().toDateString();
+  now = now.replace(/ /g, "_");
+  console.log(now);
+  fs.appendFile(`./logs/${now}.log`, "\n" + `${req.method} ${req.url} ${new Date().toUTCString()}`, 'utf8', (err) => {
+    if (err) {
+      return console.log(err);
+    }
+    return console.log('logged');
+  });
+  next();
+});
 app.use('/products', productDB);
 app.use('/articles', articleDB);
 
